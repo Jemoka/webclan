2863aeed3256e830969b9a4e2abe967dcb00ae99
Houjun Liu <houjun@jemoka.com>
2025-11-26 21:37:33 -0800

    also allow talkbank

--- server/app.py
+++ server/app.py
@@ -12,10 +12,10 @@ from pydantic import BaseModel, validator
 
 app = FastAPI()
 
-# CORS configuration for localhost and 0.0.0.0 on any port
+# CORS configuration for localhost, 0.0.0.0, and baweb.talkbank.org on any port
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1|0\.0\.0\.0)(:\d+)?",
+    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1|0\.0\.0\.0|baweb\.talkbank\.org)(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
